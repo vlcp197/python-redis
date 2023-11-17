19de4425ac90870a6117fed64fb8912c51b5d69a
vlcp197 <vlcp197@gmail.com>
2023-11-16 23:43:25 -0300

    Implement the methods logic

--- main.py
+++ main.py
@@ -21,19 +21,90 @@ Error = namedtuple('Error', ('message',))
 
 class ProtocolHandler:
 
+    def __init__(self):
+        self.handlers = {
+            '+': self.handle_simple_string,
+            '-': self.handle_error,
+            ':': self.handle_integer,
+            '$': self.handle_string,
+            '*': self.handle_array,
+            '%': self.handle_dict
+        }
+
     def handle_request(self, socket_file):
         """
             Parse a request from the client
             into it's component parts.
         """
-        pass
+        first_byte = socket_file.read(1)
+        if not first_byte:
+            raise Disconnect()
+
+        try:
+            return self.handlers[first_byte](socket_file)
+        except KeyError:
+            raise CommandError('bad request')
+
+    def handle_simple_string(self, socket_file):
+        return socket_file.readline().rstrip('\r\n')
+
+    def handle_error(self, socket_file):
+        return Error(socket_file.readline().rstrip('\r\n')) 
+
+    def handle_integer(self, socket_file):
+        return int(socket_file.readline().rstrip('\r\n'))
+
+    def handle_string(self, socket_file):
+        length = int(socket_file.readline().rstrip('\r\n'))
+        if length == -1:
+            return None
+        length += 2
+        return socket_file.read(length)[:-2]
+
+    def handle_array(self, socket_file):
+        num_elements = int(socket_file.readline().rstrip('\r\n'))
+        return [self.handle_request(socket_file) for _ in range(num_elements)]
+
+    def handle_dict(self, socket_file):
+        num_items = int(socket_file.readline().rstrip('\r\n'))
+        elements = [self.handle_request(socket_file)
+                    for _ in range(num_items * 2)]
+        return dict(zip(elements[::2], elements[1::2]))        
 
     def write_response(self, socket_file, data):
         """
             Serialize the response data
             and send it to the client
         """
-        pass
+        buf = BytesIO()
+        self._write(buf, data)
+        buf.seek(0)
+        socket_file.write(buf.getvalue())
+        socket_file.flush()
+
+    def _write(self, buf, data):
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+
+        if isinstance(data, bytes):
+            buf.write(f'${len(data)}\r\n{data}\r\n')
+        elif isinstance(data, int):
+            buf.write(f':{data}\r\n')
+        elif isinstance(data, Error):
+            buf.write(f'-{Error.message}\r\n')
+        elif isinstance(data, (list, tuple)):
+            buf.write(f'*{len(data)}\r\n')
+            for item in data:
+                self._write(buf, item)
+        elif isinstance(data, dict):
+            buf.write(f'%%{len(data)}\r\n')
+            for key in data:
+                self._write(buf, key)
+                self._write(buf, data[key])
+        elif data is None:
+            buf.write('$-1\r\n')
+        else:
+            raise CommandError(f'unrecognized type: {type(data)}')
 
 
 class Server:
@@ -50,6 +121,44 @@ class Server:
         )
         self._protocol = ProtocolHandler()
         self._kv = {}
+
+        self._commands = self.get_commands()
+
+    def get(self, key):
+        return self._kv.get(key)
+
+    def set(self, key, value):
+        self._kv[key] = value
+        return 1
+
+    def delete(self, key):
+        if key in self._kv:
+            del self._kv[key]
+            return 1
+        return 0
+
+    def flush(self):
+        kvlen = len(self._kv)
+        self._kv.clear()
+        return kvlen
+
+    def mget(self, *keys):
+        return [self._kv.get(key) for key in keys]
+
+    def mset(self, *items):
+        data = zip(items[::2], items[1::2])
+        for key, value in data:
+            self._kv[key] = value
+        return len(data)
+
+    def get_commands(self):
+        return {
+            'GET': self.get,
+            'SET': self.set,
+            'DELETE': self.delete,
+            'FLUSH': self.flush,
+            'MGET': self.get,
+            'MSET': self.set}
 
     def connection_handler(self, conn, address):
         """
@@ -78,8 +187,23 @@ class Server:
             execute the command specified,
             and pass back the return value.
         """
+        if not isinstance(data, list):
+            try:
+                data = data.split()
+            except Exception:
+                raise CommandError(
+                    'Request must be list or simple string'
+                )
 
-        pass
+        if not data:
+            raise CommandError('Missing command')
+
+        command = data[0].upper()
+        if command not in self._commands:
+            raise CommandError(
+                f'Unrecognized command: {command}')
+
+        return self._commands[command](*data[1:])
 
     def run(self):
         self._server.serve_forever()
